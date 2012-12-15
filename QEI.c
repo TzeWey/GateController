e7c10ed58241417c48fcbe1ea813565b430d4626
HardDrive <me@hion.tw>
2012-12-15 10:32:51 +0800

    Removed Debugging asm("nop") trigger points

--- QEI.c
+++ QEI.c
@@ -70,9 +70,6 @@ void QEIInterruptHandler(void)
     // Trigger callback if count reached, check handle is valid before trying
     if ((TriggerHandle != NULL) && (EncoderCount == EncoderTriggerCount))
     {
-        asm("nop");
-        asm("nop");
-        asm("nop");
         (*TriggerHandle)();
     }
 
